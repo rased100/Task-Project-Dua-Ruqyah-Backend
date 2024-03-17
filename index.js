5ce3e04eaa5e8c8fdb49ec2330567e992e51cbd3
rased <rased100@gmail.com>
2024-03-18 01:59:30 +0600

    database

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const sqlite3 = require("sqlite3").verbose();
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+// database connection
+const database = new sqlite3.Database("./dua_main.sqlite");
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// get
+app.get("/", (req, res) => {
+  res.send("duaruqyah node server running");
+});
+
+// server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
